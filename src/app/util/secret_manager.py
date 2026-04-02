238bd12ef8f77be1c3520433d599cc3b3c2084ac
Mohamed Sharif <e0540641@u.nus.edu>
2026-04-02 16:44:52 +0800

    Update method used to retrieve secret

--- src/app/util/secret_manager.py
+++ src/app/util/secret_manager.py
@@ -1,9 +1,16 @@
 import json
-import os
 import boto3
 from botocore.exceptions import ClientError
 
 def get_secret(secret_arn: str, secret_key: str) -> str:
     client = boto3.client("secretsmanager")
+
     response = client.get_secret_value(SecretId=secret_arn)
-    return response[secret_key]
+
+    secret_string = response.get("SecretString")
+    if not secret_string:
+        raise ValueError("SecretString is empty")
+
+    secret_dict = json.loads(secret_string)
+
+    return secret_dict[secret_key]
